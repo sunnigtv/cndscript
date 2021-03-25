3bb210bb3bcd327697b7793273e72534928d80e0
sunnigtv <61459888+sunnigtv@users.noreply.github.com>
2021-03-25 20:49:19 +0900

    Add files via upload

---
+++ cpp/sundro.cpp
@@ -0,0 +1,9 @@
+﻿#include "cndroScript.h"
+
+int main(void)
+{
+	es::iao::output::print('k', 'i', 'm', 's', 'u', 'm', 'i', 'n', ' ', ' ', ' ', ' ', '\n');
+	char ss = 0;
+	es::iao::lnput::input(ss);
+	es::iao::output::print('k', 'i', 'm', 's', 'u', 'm', 'i', 'n', ' ', ' ', ' ', ' ', '\n');
+}
